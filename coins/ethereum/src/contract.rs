b489eb3199db465e6354f7901d7a325601cb07bd
noot <elizabethjbinks@gmail.com>
2022-08-07 01:09:15 -0400

    start testing router contract

--- coins/ethereum/src/contract.rs
+++ coins/ethereum/src/contract.rs
@@ -17,6 +17,12 @@ abigen!(
   event_derives(serde::Deserialize, serde::Serialize),
 );
 
+abigen!(
+  Router,
+  "./artifacts/Router.sol/Router.json",
+  event_derives(serde::Deserialize, serde::Serialize),
+);
+
 pub async fn deploy_schnorr_verifier_contract(
   client: Arc<SignerMiddleware<Provider<Http>, LocalWallet>>,
 ) -> Result<schnorr_mod::Schnorr<SignerMiddleware<Provider<Http>, LocalWallet>>> {
@@ -27,6 +33,19 @@ pub async fn deploy_schnorr_verifier_contract(
   let factory = ContractFactory::new(abi, bin.into_bytes().unwrap(), client.clone());
   let contract = factory.deploy(())?.send().await?;
   let contract = Schnorr::new(contract.address(), client);
+  Ok(contract)
+}
+
+pub async fn deploy_router_contract(
+  client: Arc<SignerMiddleware<Provider<Http>, LocalWallet>>,
+) -> Result<router_mod::Router<SignerMiddleware<Provider<Http>, LocalWallet>>> {
+  let path = "./artifacts/Router.sol/Router.json";
+  let artifact: ContractBytecode = serde_json::from_reader(File::open(path).unwrap()).unwrap();
+  let abi = artifact.abi.unwrap();
+  let bin = artifact.bytecode.unwrap().object;
+  let factory = ContractFactory::new(abi, bin.into_bytes().unwrap(), client.clone());
+  let contract = factory.deploy(())?.send().await?;
+  let contract = Router::new(contract.address(), client);
   Ok(contract)
 }
 
@@ -41,6 +60,28 @@ pub async fn call_verify(
       params.message,
       params.s.to_bytes().into(),
       params.e.to_bytes().into(),
+    )
+    .call()
+    .await?
+  {
+    Ok(())
+  } else {
+    Err(eyre!(EthereumError::VerificationError))
+  }
+}
+
+pub async fn call_router_execute(
+  contract: &router_mod::Router<SignerMiddleware<Provider<Http>, LocalWallet>>,
+  txs: Vec<router_mod::Transaction>,
+  signature: &ProcessedSignature,
+) -> Result<()> {
+  if contract
+    .execute(
+      txs,
+      signature.parity + 27,
+      signature.px.to_bytes().into(),
+      signature.s.to_bytes().into(),
+      signature.e.to_bytes().into(),
     )
     .call()
     .await?
