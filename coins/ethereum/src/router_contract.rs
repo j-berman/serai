387034a04f92cea917c087868d5645d105eb019c
noot <elizabethjbinks@gmail.com>
2023-01-31 15:27:32 -0500

    hardcode pubkey parity into contract, update tests

--- coins/ethereum/src/router_contract.rs
+++ coins/ethereum/src/router_contract.rs
@@ -4,27 +4,13 @@ use ethers::{contract::ContractFactory, prelude::*, solc::artifacts::contract::C
 use eyre::Result;
 use std::{convert::From, fs::File, sync::Arc};
 
-// abigen!(
-//   Router,
-//   "./artifacts/Router.sol/Router.json",
-//   event_derives(serde::Deserialize, serde::Serialize),
-// );
-
-impl From<&PublicKey> for RpublicKey {
-  fn from(public_key: &PublicKey) -> Self {
-    RpublicKey { parity: public_key.parity, px: public_key.px.to_bytes().into() }
-  }
-}
-
 impl From<&ProcessedSignature> for Rsignature {
   fn from(signature: &ProcessedSignature) -> Self {
     Rsignature { s: signature.s.to_bytes().into(), e: signature.e.to_bytes().into() }
   }
 }
 
-pub async fn deploy_router_contract<M: Middleware + 'static>(
-  client: Arc<M /*SignerMiddleware<Provider<Http>, LocalWallet>*/>,
-) -> Result<Router<M>> {
+pub async fn deploy_router_contract<M: Middleware + 'static>(client: Arc<M>) -> Result<Router<M>> {
   let path = "./artifacts/Router.sol/Router.json";
   let artifact: ContractBytecode = serde_json::from_reader(File::open(path).unwrap()).unwrap();
   let abi = artifact.abi.unwrap();
@@ -39,7 +25,7 @@ pub async fn router_set_public_key<M: Middleware + 'static>(
   contract: &Router<M>,
   public_key: &PublicKey,
 ) -> std::result::Result<Option<TransactionReceipt>, eyre::ErrReport> {
-  let tx = contract.set_public_key(public_key.into());
+  let tx = contract.set_public_key(public_key.px.to_bytes().into());
   let pending_tx = tx.send().await?;
   let receipt = pending_tx.await?;
   Ok(receipt)
@@ -50,7 +36,7 @@ pub async fn router_update_public_key<M: Middleware + 'static>(
   public_key: &PublicKey,
   signature: &ProcessedSignature,
 ) -> std::result::Result<Option<TransactionReceipt>, eyre::ErrReport> {
-  let tx = contract.update_public_key(public_key.into(), signature.into());
+  let tx = contract.update_public_key(public_key.px.to_bytes().into(), signature.into());
   let pending_tx = tx.send().await?;
   let receipt = pending_tx.await?;
   Ok(receipt)
